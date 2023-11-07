78cfb4b198973c3f30601aa44036cc40b7a627fe
BaixiGuo <steven20001108@gmail.com>
2023-11-06 17:01:53 -0800

    Change to SQLite Version Code

--- sql script/create_table.sql
+++ sql script/create_table.sql
@@ -1,88 +1,63 @@
--- For CSE 111 Code
-CREATE DATABASE meetminder;
-
-USE meetminder;
-
 CREATE TABLE user (
-    userid   INT NOT NULL AUTO_INCREMENT,
-    username VARCHAR(255) NOT NULL,
-    email    VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    
-    PRIMARY KEY (userid)
+    userid   INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email    TEXT NOT NULL,
+    password TEXT NOT NULL
 );
 
 CREATE TABLE meeting (
-    meetingid   INT NOT NULL AUTO_INCREMENT,
-    title       VARCHAR(255) NOT NULL,
+    meetingid   INTEGER PRIMARY KEY,
+    title       TEXT NOT NULL,
     description TEXT,
-    time        DATETIME NOT NULL,
-    location    VARCHAR(255) NOT NULL,
-    organizerid INT NOT NULL,
-    
-    PRIMARY KEY (meetingid),
+    time        DATE NOT NULL,
+    location    TEXT NOT NULL,
+    organizerid INTEGER NOT NULL,
     FOREIGN KEY (organizerid) REFERENCES user(userid)
 );
 
 CREATE TABLE agenda (
-    agendaid INT NOT NULL AUTO_INCREMENT,
-    topic    VARCHAR(255) NOT NULL,
-    duration INT,
-    meetingid INT NOT NULL,
-    
-    PRIMARY KEY (agendaid),
+    agendaid INTEGER PRIMARY KEY,
+    topic    TEXT NOT NULL,
+    duration INTEGER,
+    meetingid INTEGER NOT NULL,
     FOREIGN KEY (meetingid) REFERENCES meeting(meetingid)
 );
 
 CREATE TABLE attendee (
-    attendeeid INT NOT NULL AUTO_INCREMENT,
-    userid     INT NOT NULL,
-    meetingid  INT NOT NULL,
-    status     VARCHAR(255) NOT NULL,
-    
-    PRIMARY KEY (attendeeid),
+    attendeeid INTEGER PRIMARY KEY,
+    userid     INTEGER NOT NULL,
+    meetingid  INTEGER NOT NULL,
+    status     TEXT NOT NULL,
     FOREIGN KEY (userid) REFERENCES user(userid),
     FOREIGN KEY (meetingid) REFERENCES meeting(meetingid)
 );
 
 CREATE TABLE notification (
-    notificationid INT NOT NULL AUTO_INCREMENT,
-    userid         INT NOT NULL,
+    notificationid INTEGER PRIMARY KEY,
+    userid         INTEGER NOT NULL,
     message        TEXT,
-    timestamp      DATETIME NOT NULL,
-    
-    PRIMARY KEY (notificationid),
+    timestamp      DATE NOT NULL,
     FOREIGN KEY (userid) REFERENCES user(userid)
 );
 
 CREATE TABLE room (
-    roomid   INT NOT NULL AUTO_INCREMENT,
-    name     VARCHAR(255) NOT NULL,
-    capacity INT NOT NULL,
-    resourceid INT NOT NULL,
-    
-    PRIMARY KEY (roomid),
+    roomid   INTEGER PRIMARY KEY,
+    name     TEXT NOT NULL,
+    capacity INTEGER NOT NULL,
+    resourceid INTEGER NOT NULL,
     FOREIGN KEY (resourceid) REFERENCES resource(resourceid)
 );
 
 CREATE TABLE resource (
-    resourceid   INT NOT NULL AUTO_INCREMENT,
-    resourcename VARCHAR(255) NOT NULL,
-    description  TEXT,
-    
-    PRIMARY KEY (resourceid)
+    resourceid   INTEGER PRIMARY KEY,
+    resourcename TEXT NOT NULL,
+    description  TEXT
 );
 
 CREATE TABLE meetingresource (
-    meetingresourceid INT NOT NULL AUTO_INCREMENT,
-    meetingid         INT,
-    resourceid        INT,
-    
-    PRIMARY KEY (meetingresourceid),
+    meetingresourceid INTEGER PRIMARY KEY,
+    meetingid         INTEGER,
+    resourceid        INTEGER,
     FOREIGN KEY (meetingid) REFERENCES meeting(meetingid),
     FOREIGN KEY (resourceid) REFERENCES resource(resourceid)
 );
-
-SHOW TABLES;
-
-DESCRIBE user;
